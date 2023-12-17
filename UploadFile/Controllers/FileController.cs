30d5149090c1a2a3e155e1809d6031e12167077f
zeraati <zeraatimail@gmail.com>
2023-12-17 13:32:33 +0330

    GetFileNames

--- UploadFile/Controllers/FileController.cs
+++ UploadFile/Controllers/FileController.cs
@@ -1,58 +1,126 @@
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
 
-namespace UploadFile.Controllers
+namespace UploadFile.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class FileController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class FileController : ControllerBase
-    {
-        [HttpPost("Upload")]
-        public async Task<IActionResult> Upload(IFormFile file)
-        {
-            try
-            {
-                var folderName = Path.Combine(nameof(UploadFile));
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                if (file.Length > 0)
-                {
-                    var fileName = file.FileName;
-                    var fullPath = Path.Combine(pathToSave, fileName);
-                    var shortPath = Path.Combine(folderName, fileName);
+	[HttpPost("[action]")]
+	public async Task<IActionResult> Upload(string? FileName, IFormFile file)
+	{
+		try
+		{
+			var folderName = Path.Combine(nameof(UploadFile));
+			var folderPath= Path.Combine(Directory.GetCurrentDirectory(), folderName);
+			if (file.Length > 0)
+			{
+				if (string.IsNullOrEmpty(FileName) == false && FileName.ToUpper() == "GUID") FileName = Guid.NewGuid().ToString();
+				var fileName = string.IsNullOrEmpty(FileName) ? file.FileName : FileName + Path.GetExtension(file.FileName);
+				var fullPath = Path.Combine(folderPath, fileName);
+				var shortPath = Path.Combine(folderName, fileName);
 
-                    using (var stream = new FileStream(fullPath, FileMode.OpenOrCreate))
-                    {
-                        file.CopyTo(stream);
-                    }
-                    return Ok(new { path=shortPath });
-                }
-                else
-                {
-                    return BadRequest();
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex}");
-            }
-        }
+				if (System.IO.File.Exists(fullPath) == true) System.IO.File.Delete(fullPath);
 
-        [HttpGet("DownloadContent/{FileName}")]
-        public async Task<IActionResult> Download(string FileName= "family.json")
-        {
-            try
-            {
-                var folderName = Path.Combine(nameof(UploadFile));
-                var pathToRead = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                var fileFullPath = Path.Combine(pathToRead,FileName);
-                var readContent = System.IO.File.ReadAllText(fileFullPath);
-                //var jsonContent = JArray.Parse(readContent);
-                return Ok(new { content= readContent });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex}");
-            }
-        }
-    }
+				using (var stream = new FileStream(fullPath, FileMode.Create))
+				{
+					await file.CopyToAsync(stream);
+				}
+				return Ok(new { fileName });
+			}
+			else
+			{
+				return BadRequest();
+			}
+		}
+		catch (Exception ex)
+		{
+			return StatusCode(500, $"Internal server error: {ex}");
+		}
+	}
+
+	[HttpDelete("[action]/{FileName}")]
+	public async Task<IActionResult> Delete(string FileName)
+	{
+		try
+		{
+			var folderName = Path.Combine(nameof(UploadFile));
+			var folderPath = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+			var fullPath = Path.Combine(folderPath, FileName);
+			if (System.IO.File.Exists(fullPath) == true) System.IO.File.Delete(fullPath);
+
+			return Ok();
+		}
+		catch (Exception ex)
+		{
+			return StatusCode(500, $"Internal server error: {ex}");
+		}
+	}
+
+	[HttpGet("[action]/{Filter}")]
+	public async Task<IActionResult> GetFileNames(string Filter)
+	{
+		if (string.IsNullOrEmpty(Filter) == true) Filter = "*.*";
+
+		try
+		{
+			var folderName = Path.Combine(nameof(UploadFile));
+			var folderPath = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+			var filePaths = Directory.GetFiles(folderPath, Filter, SearchOption.TopDirectoryOnly);
+
+			var result = filePaths.Select(x=>x.Replace(folderPath+"\\","")).ToList();
+
+			return Ok(result);
+		}
+		catch (Exception ex)
+		{
+			return StatusCode(500, $"Internal server error: {ex}");
+		}
+	}
+
+	[HttpGet("DownloadContent/{FileName}")]
+	public async Task<IActionResult> Download(string FileName = "family.json")
+	{
+		try
+		{
+			var folderName = Path.Combine(nameof(UploadFile));
+			var pathToRead = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+			var fullPath = Path.Combine(pathToRead, FileName);
+			var readContent = await System.IO.File.ReadAllTextAsync(fullPath);
+			return Ok(new { content = readContent });
+		}
+		catch (Exception ex)
+		{
+			return StatusCode(500, $"Internal server error: {ex}");
+		}
+	}
+
+	[HttpGet("Download/{FileName}")]
+	public async Task<IActionResult> DownloadFile(string FileName)
+	{
+		if (string.IsNullOrEmpty(FileName)) return BadRequest();
+
+		try
+		{
+			var folderName = Path.Combine(nameof(UploadFile));
+			var pathToRead = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+			var fullPath = Path.Combine(pathToRead, FileName);
+			var extention = Path.GetExtension(FileName).Remove(0, 1);
+			var fileByte = await System.IO.File.ReadAllBytesAsync(fullPath);
+
+			switch (extention)
+			{
+				case "png":
+					return File(fileByte, "image/png");
+				case "jpg":
+					return File(fileByte, "image/jpg");
+				default:
+					return File(fileByte, "image/jpg");
+			}
+		}
+
+		catch (Exception ex)
+		{
+			return StatusCode(500, $"Internal server error: {ex}");
+		}
+	}
 }
--- UploadFile/Program.cs
+++ UploadFile/Program.cs
@@ -1,23 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "Cors",builder =>{ builder.AllowAnyOrigin();});
+});
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
+app.UseHttpsRedirection();
+app.UseCors("Cors");
 app.UseAuthorization();
 
 app.MapControllers();
